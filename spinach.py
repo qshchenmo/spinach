86daaa2a764a15ace5e012d6df049ec553edff14
qshchenmo <32218502+qshchenmo@users.noreply.github.com>
2023-04-16 08:50:29 +0800

    去调正则的结尾
    这样可以接受额外的文字

--- spinach.py
+++ spinach.py
@@ -11,7 +11,7 @@ class Bocai_machine(object):
         self.my_account  = discuz.Discuz()
         self.page = 1
         self.record = {}
-        self.record_pattern = re.compile(r"[\r\n](\d{2,3}|1000)\s+on\s+([AB])$", re.IGNORECASE)
+        self.record_pattern = re.compile(r"[\r\n](\d{2,3}|1000)\s+on\s+([AB])", re.IGNORECASE)
 
         if not config.USERNAME:
             config.USERNAME = raw_input("请输入用户名：")
